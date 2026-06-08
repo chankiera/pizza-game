d91b2f391623f6ab977781386e89f2c7035e7a18
Alex Sung <alex.won.sung@gmail.com>
2026-06-07 17:43:53 -0700

    Can move around pizza scene, enter a pizza toppings view and exit

---
+++ Assets/Scripts/ToppingsStation.cs
@@ -0,0 +1,82 @@
+using UnityEngine;
+
+public class ToppingsStation : MonoBehaviour
+{
+    public GameObject enterPrompt;
+    public GameObject fpsCamera;
+    public GameObject pizzaCamera;
+    public GameObject playerBody;
+    public PlayerMovement playerMovement;
+    public MouseBehavior mouseMovement;
+
+    private bool playerNearby;
+    private bool inPizzaMode;
+
+
+    void Start()
+    {
+        inPizzaMode = false;
+        EnterFPSMode();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (inPizzaMode && Input.GetKeyDown(KeyCode.E))
+        {
+            EnterFPSMode();
+            inPizzaMode = false;
+        }
+
+        else if (playerNearby && Input.GetKeyDown(KeyCode.E))
+        {
+            EnterPizzaMode();
+            inPizzaMode = true;
+        }
+    }
+
+    void EnterPizzaMode()
+    {
+        enterPrompt.SetActive(false);
+        fpsCamera.SetActive(false);
+        pizzaCamera.SetActive(true);
+        playerMovement.enabled = false;
+        mouseMovement.enabled = false;
+        playerBody.SetActive(false);
+
+        Cursor.lockState = CursorLockMode.None;
+        Cursor.visible = true;
+    }
+
+    void EnterFPSMode()
+    {
+        pizzaCamera.SetActive(false);
+        fpsCamera.SetActive(true);
+        playerMovement.enabled = true;
+        mouseMovement.enabled = true;
+        enterPrompt.SetActive(false);
+        playerBody.SetActive(true);
+
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            Debug.Log("Entered");
+            enterPrompt.SetActive(true);
+            playerNearby = true;
+        }
+    }
+
+    void OnTriggerExit(Collider other)
+    {
+        if (other.CompareTag("Player"))
+        {
+            playerNearby = false;
+            enterPrompt.SetActive(false);
+        }
+    }
+}
