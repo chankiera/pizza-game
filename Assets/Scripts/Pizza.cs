e0051df53a08c1124c017c62696d689acca3b231
Alex Sung <alex.won.sung@gmail.com>
2026-06-09 06:38:45 -0700

    Added pizzeria

--- Assets/Scripts/Pizza.cs
+++ Assets/Scripts/Pizza.cs
@@ -2,24 +2,42 @@ using UnityEngine;
 
 public class Pizza : MonoBehaviour
 {
-    public GameObject sliceVisual;
+    public GameObject FourSlices;
+    public GameObject SixSlices;
+    public GameObject EightSlices;
 
-    private bool sliced;
+    private int slices;
 
     void Start()
     {
-        // sliceVisual.SetActive(false);
+        slices = 0;
     }
 
     public void Slice()
     {
-        if (!PizzaCutter.cutterSelected || sliced)
+        if (!PizzaCutter.cutterSelected || slices == 8)
         {
             return;
         }
 
-        sliced = true;
-        sliceVisual.SetActive(true);
+        if (slices == 0)
+        {
+            FourSlices.SetActive(true);
+            slices = 4;
+        }
+        else if (slices == 4)
+        {
+            FourSlices.SetActive(false);
+            SixSlices.SetActive(true);
+            slices = 6;
+        }
+        else if (slices == 6)
+        {
+            SixSlices.SetActive(false);
+            EightSlices.SetActive(true);
+            slices = 8;
+        }
+
         PizzaCutter.cutterSelected = false;
     }
 
